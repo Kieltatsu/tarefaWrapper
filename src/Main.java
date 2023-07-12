a76fc03c8692e2c1948dd47587a3fe5e5605a106
Kieltatsu <kieltatsuno@gmail.com>
2023-07-12 14:54:19 -0300

    mod9

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main ( String args []) {
+        int num1 = 10;
+        Double num2 = (double) num1;
+
+
+        System.out.println(num2);
+    }
+
+}
